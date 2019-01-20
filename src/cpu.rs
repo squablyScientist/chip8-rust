92ed019bd326dac1178f2792c71830307aaaa11d
Collin Tod <squablesquable@gmail.com>
2019-01-20 16:27:46 -0500

    Finish initialization of CPU status
    The chip-8 can read the roms into their correct places in memory, load
    the fontset, and begin execution.
    
    TODO:
    	work on some more debugging tools like a register viewer
    	Interpret every instruction
    	Write tests to make absolutely sure that the CPU functions
    	Implement display somehow(ncurses?)

--- src/cpu.rs
+++ src/cpu.rs
@@ -154,17 +154,35 @@ impl CHIP8 {
         }
     }
 
-    pub fn load_program(&self, filename: String) {
-        let mut file = File::open(filename).unwrap();
-        let mut buf = [0u8; 0xFFF-0x600];
-        file.read(&mut buf).unwrap();
+    pub fn load_program(&mut self, filename: String) {
 
-        for i in 0..0xFFF-0x600 {
-            println!("{}", buf[i]);
+        let mut file = File::open(filename).unwrap();
+        let mut buf = Vec::new();
+
+        // Reads the entire ROM file 
+        let bytes_read: usize = file.read_to_end(&mut buf).unwrap();
+
+        for i in 0..bytes_read{
+            self.mem[i + 0x200] = buf[i];
         }
     }
+
+    // Function to dump the entire memory of the chip8 in xxd format. 
+    // TODO: allow for memory ranges and ASCII printing.
+    pub fn dump_mem(&self) -> String {
+        let mut mem_dump = String::new();
+        for i in (0..4096).step_by(16) {
+            mem_dump.push_str(&format!("{:03x}:   ", i));
+            for j in 0..8 {
+                mem_dump.push_str(&format!("{:02x}{:02x} ", 
+                                          self.mem[i+j], self.mem[i+j+1]));
+            }
+            mem_dump.push_str("\n");
+        }
+        mem_dump
+    }
     
-    pub fn cycle(&self) {
+    pub fn cycle(&mut self) {
         // Fetch
         let pc = self.PC;
         let mem = self.mem;
@@ -175,6 +193,23 @@ impl CHIP8 {
         opcode |= self.mem[(pc as usize) + 1] as u16;
 
         // Decode
+        match opcode & 0xF000 {
+
+            // JP addr: Sets the pc to the lower 3 nibbles of the instruction.
+            0x1000 => self.PC = ((opcode << 4) >> 4),
+
+            // CALL addr: puts return addr on the stack and jumps.
+            0x2000 => {
+                self.stack[self.SP as usize] = self.PC;
+                self.SP = self.SP + 1;
+                self.PC = ((opcode << 4) >> 4)
+            }
+
+            // TODO: implement rest of instructions
+
+
+            _ => panic!(),
+        }
         // Execute
         
         // update timers
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 mod cpu;
 fn main() {
-    println!("Hello, world!");
+    let mut chip8 = cpu::CHIP8::init();
+    chip8.load_program(String::from("roms/maze.ch8"));
+    println!("{}", chip8.dump_mem());
 }
+
