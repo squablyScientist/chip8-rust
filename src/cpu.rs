f730941c94b58c09d0759c5ceb748a10ca83fdf4
Collin Tod <squablesquable@gmail.com>
2019-01-21 16:25:26 -0500

    implement all non-display instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,4 +1,6 @@
+extern crate rand;
 use std::fs::File;
+//use self::rand::random;
 use std::io::Read;
 
 pub const FONT: [u8; 80] = [
@@ -113,10 +115,10 @@ pub struct CHIP8 {
     pub  sound: u8,     
 
     // Program counter
-    pub  PC: u16,       
+    pub  pc: u16,       
 
     // Stack pointer
-    pub  SP: u8,        
+    pub  sp: u8,        
     
     // The memory accessible by the CHIP-8 CPU
     pub mem: [u8; 4096],
@@ -145,8 +147,8 @@ impl CHIP8 {
             I: 0,
             delay: 0,
             sound: 0,
-            PC: 0x200,
-            SP: 0,
+            pc: 0x200,
+            sp: 0,
             mem: mem,
             stack: [0; 16],
             display: [[false; 64]; 32],
@@ -181,37 +183,304 @@ impl CHIP8 {
         }
         mem_dump
     }
-    
+
     pub fn cycle(&mut self) {
         // Fetch
-        let pc = self.PC;
-        let mem = self.mem;
+        let pc = self.pc;
 
-        let mut opcode: u16 = 0;
-
-        opcode = ((self.mem[pc as usize]) as u16) << 8;
+        let mut opcode: u16  = ((self.mem[pc as usize]) as u16) << 8;
         opcode |= self.mem[(pc as usize) + 1] as u16;
 
-        // Decode
+        // Decode & Execute 
         match opcode & 0xF000 {
 
+            
+            // 0 instructions
+            0x0000 => {
+                match opcode & 0x00FF {
+
+                    // CLS: Clears the screen
+                    0x00E0 => {
+                        //TODO implement for clearing
+                    },
+
+                    // RET: returns from a subroutine
+                    0x00EE => {
+                        self.pc = self.stack[self.sp as usize];
+                        self.sp -= 1;
+                    }
+
+                    _ => panic!(),
+                }
+            },
+
             // JP addr: Sets the pc to the lower 3 nibbles of the instruction.
-            0x1000 => self.PC = ((opcode << 4) >> 4),
+            0x1000 => self.pc = opcode & 0xFF,
 
             // CALL addr: puts return addr on the stack and jumps.
             0x2000 => {
-                self.stack[self.SP as usize] = self.PC;
-                self.SP = self.SP + 1;
-                self.PC = ((opcode << 4) >> 4)
+                self.sp += 1;
+                self.stack[self.sp as usize] = self.pc;
+                self.pc = opcode & 0xFFF;
+            },
+
+            // SE Vx, byte: Skips next instruction if Vx == byte
+            0x3000 => {
+                let x : usize = ((opcode & 0x0F00) >> 8) as usize;
+                let byte : u8 = (opcode & 0x00FF) as u8;
+                if self.V[x] == byte {
+                    self.pc += 2;
+                }
+                self.pc += 2;
+            },
+
+            // SNE Vx, byte: Skips next instruction if Vx != byte
+            0x4000 => {
+                let x : usize = ((opcode & 0x0F00) >> 8) as usize;
+                let byte : u8 = (opcode & 0x00FF) as u8;
+                if self.V[x] != byte {
+                    
+                    // Skips the next instruction
+                    self.pc += 2;
+                }
+                self.pc += 2;
+            },
+            
+            // SE Vx Vy : Skips next instruction if Vx == Vy
+            0x5000 => {
+                let x : usize = ((opcode & 0x0F00) >> 8) as usize;
+                let y : usize = ((opcode & 0x00F0) >> 4) as usize;
+
+                if self.V[x] == self.V[y] {
+                    self.pc += 2;
+                }
+                self.pc += 2;
+            },
+
+            // LD Vx, byte: sets the value in Vx to byte
+            0x6000 => {
+                let x : usize = ((opcode & 0x0F00) >> 8) as usize;
+                let byte : u8 = (opcode & 0x00FF) as u8;
+                self.V[x] = byte;
+                self.pc += 2;
+            },
+
+            // ADD Vx, byte : Vx += byte
+            0x7000 => {
+                let x : usize = ((opcode & 0x0F00) >> 8) as usize;
+                let byte : u8 = (opcode & 0x00FF) as u8;
+                self.V[x] += byte;
+                self.pc += 2;
+            },
+
+            // Various Arithmetic and Bitwise Operations
+            0x8000 => {
+
+                // These arguments are constant for all 0x8xy? instructions
+                let x : usize = ((opcode & 0x0F00) >> 8) as usize;
+                let y : usize = ((opcode & 0x00F0) >> 4) as usize;
+
+                match opcode & 0x000F {
+
+                    // LD Vx, Vy : Sets Vx = Vy
+                    0x0000 => self.V[x] = self.V[y],
+
+                    // OR Vx, Vy : Vx |= Vy
+                    0x0001 => self.V[x] |= self.V[y],
+
+                    // AND Vx, Vy : Vx &= Vy
+                    0x0002 => self.V[x] &= self.V[y],
+
+                    // XOR Vx, Vy : Vx ^= Vy
+                    0x0003 => self.V[x] ^= self.V[y],
+
+                    // ADD Vx, Vy : Vx += Vy. Sets VF to 1 if overflow occurs, to 0 o/wise
+                    0x0004 => {
+                        let sum : u16 = self.V[x] as u16 + self.V[y] as u16;
+                        if sum > 255 {
+                            self.V[0xF] = 1;
+                        }
+                        else {
+                            self.V[0xF] = 0;
+                        }
+                        self.V[x] = (sum & 0x00FF) as u8;
+                    }
+
+                    // SUB Vx, Vy : Vx -= Vy. If Vx > Vy then VF is set to 1, 0 o/wise
+                    0x0005 => {
+                        if self.V[x] > self.V[y] {
+                            self.V[0xF] = 1;
+                        }
+                        else{
+                            self.V[0xF] = 0;
+                        }
+
+                        self.V[x] -= self.V[y];
+                    },
+
+                    // SHR Vx (, Vy) : Shifts Vx right by 1, setting VF to 1 if the lsb was 1
+                    // before shift
+                    0x0006 => {
+
+                        // If the least significant bit is 1
+                        if self.V[x] & 0x0001 == 1 {
+                            self.V[0xF] = 1;
+                        }
+                        else {
+                            self.V[0xF] = 0;
+                        }
+
+                        self.V[x] = self.V[x] >> 1;
+                    },
+
+                    // SUBN Vx, Vy : Vx = Vy - Vx, sets VF = NOT BORROW
+                    0x0007 => {
+                        if self.V[y] > self.V[x] {
+                            self.V[0xF] = 1;
+                        }
+                        else {
+                            self.V[0xF] = 0;
+                        }
+
+                        self.V[x] = self.V[y] - self.V[x];
+                    },
+
+                    // SHR Vx (,Vy) : Shifts Vx right and sets VF 1 if msb was 1 before shift
+                    0x000E => {
+
+                        // If the most significant bit is 1 
+                        if self.V[x] & 0x80 != 0 {
+                            self.V[0xF] = 1;
+                        }
+                        else {
+                            self.V[0xF] = 0;
+                        }
+                        self.V[x] = self.V[x] << 1;
+                    },
+
+                    _ => panic!(),
+                };
+
+                self.pc += 2;
+            },
+            
+            // SNE Vx, Vy: Skips the next instruction if Vx != Vy
+            0x9000 => {
+                let x: usize = ((opcode & 0x0F00) >> 8) as usize;
+                let y: usize = ((opcode & 0x00F0) >> 4) as usize;
+                if self.V[x] != self.V[y] {
+                    self.pc += 2;
+                }
+                self.pc += 2;
+            },
+
+            // LD I, addr: Loads a 12-bit address into I
+            0xA000 => {
+                self.I = opcode & 0x0FFF;
+                self.pc += 2;
             }
 
-            // TODO: implement rest of instructions
+            // JP V0, addr: Sets the pc to V0 + addr
+            0xB000 => self.pc = (opcode & 0x0FFF) + (self.V[0] as u16),
 
+            // RND Vx, byte: Vx = random byte AND byre
+            0xC000 => {
+                let x: usize = ((opcode & 0x0F00) >> 8) as usize;
+                let byte: u8 = (opcode & 0x00FF) as u8;
+                let r = rand::random::<u8>();
+                self.V[x] = byte & r;
+                self.pc += 2;
+            },
+
+            // TODO: implement displaying sprite to a screen 
+            0xD000 => {},
+
+            // Keypad skip instructions
+            0xE000 => {
+                let val_of_x: usize = self.V[((opcode & 0x0F00) >> 8) as usize] as usize; 
+
+                match opcode & 0x00FF {
+
+                    // SKP Vx : Skips the next instruction iff they key with the value in Vx is
+                    // pressed.
+                    0x009E => {
+                        if self.keypad[val_of_x] == true {
+                            self.pc += 2;
+                        }
+                        self.pc += 2;
+                    }
+
+                    // SKNP Vx : Skips the next instruction iff the keypad with the value in Vx is
+                    // not pressed.
+                    0x00A1 => {
+                        if self.keypad[val_of_x] == false {
+                            self.pc += 2;
+                        }
+                        self.pc += 2;
+                    }
+
+                    _ => panic!(),
+                }
+
+            },
+
+
+            0xF000 => {
+                let x: usize = ((opcode & 0x0F00) >> 8) as usize;
+
+                match opcode & 0x00FF {
+                    
+                    // LD Vx, DT: Puts the value of the delay timer into Vx
+                    0x0007 => self.V[x] = self.delay,
+
+                    // LD Vx, K: Waits for a key press and puts the value of key in Vx
+                    // TODO: implement
+                    0x000A => {},
+
+                    // LD DT, Vx: Loads the delay timer with the value in Vx
+                    0x0015 => self.delay = self.V[x],
+
+                    // LD ST, Vx: Loads the sound timer with the value in Vx
+                    0x0018 => self.sound = self.V[x],
+
+                    // ADD I, Vx: I += Vx
+                    0x001E => self.I += self.V[x] as u16,
+
+                    // LD I, Vx: Sets I to the location of the sprite for digit Vx
+                    0x0029 => {
+                        self.I = (0x50 + self.V[x]*5) as u16;
+                    },
+
+                    // LD B, Vx: Stores the decimal representation of Vx at: 100s:I, 10s:I+1,
+                    // 1s:I+2.
+                    0x0033 => {
+                        self.mem[(self.I) as usize] = self.V[x] / 100;
+                        self.mem[(self.I + 1) as usize] = (self.V[x] % 100) / 10;
+                        self.mem[(self.I + 2) as usize] = self.V[x] % 10;
+                    },
+
+                    // LD [I], Vx : Stores registers V0 thru Vx in memory starting at addr I
+                    0x0055 => {
+                        for i in 0..(x + 1) {
+                            self.mem[(self.I + (i as u16)) as usize] = self.V[i];
+                        }
+                    },
+
+                    // LD Vx, [I] : Reads registers V0 thru Vx from memory starting at addr I
+                    0x0065 => {
+                        for i in 0..(x + 1) {
+                            self.V[i] = self.mem[(self.I + (i as u16)) as usize];
+                        }
+                    }
+
+                    _ => panic!(),
+                }
+                self.pc += 2;
+            },
 
             _ => panic!(),
         }
-        // Execute
         
-        // update timers
+        // TODO update timers
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ mod cpu;
 fn main() {
     let mut chip8 = cpu::CHIP8::init();
     chip8.load_program(String::from("roms/maze.ch8"));
-    println!("{}", chip8.dump_mem());
+    loop {
+        chip8.cycle();
+        println!("{}", chip8.dump_mem());
+    }
 }
 
