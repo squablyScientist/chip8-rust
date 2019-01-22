341631d4d8d9b2be6dcf09e10093cd7751b23ff3
Collin Tod <squablesquable@gmail.com>
2019-01-21 21:17:40 -0500

    Add cycle time

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+use std::{thread, time};
 mod cpu;
 fn main() {
     let mut chip8 = cpu::CHIP8::init();
-    chip8.load_program(String::from("roms/Chip8 Picture.ch8"));
+    chip8.load_program(String::from("roms/BC_test.ch8"));
     loop {
         println!("{}", chip8.dump_reg());
         chip8.cycle();
         println!("{}", chip8.dump_display());
+        thread::sleep(time::Duration::from_millis(100));
     }
 }
 
