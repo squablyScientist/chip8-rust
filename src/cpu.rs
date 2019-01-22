6818b912f891915280377ceb8270d4adf0b3dad4
Collin Tod <squablesquable@gmail.com>
2019-01-21 19:39:24 -0500

    Fix 0x1??? bug: only reading low byte

--- src/cpu.rs
+++ src/cpu.rs
@@ -245,7 +245,7 @@ impl CHIP8 {
             },
 
             // JP addr: Sets the pc to the lower 3 nibbles of the instruction.
-            0x1000 => self.pc = opcode & 0xFF,
+            0x1000 => self.pc = opcode & 0x0FFF,
 
             // CALL addr: puts return addr on the stack and jumps.
             0x2000 => {
@@ -427,6 +427,8 @@ impl CHIP8 {
                 let vx = self.V[((opcode & 0x0F00) >> 8) as usize];
                 let vy = self.V[((opcode & 0x00F0) >> 8) as usize];
                 let mut pixel: u8 = 0;
+
+                println!("Displaying sprite of height {} on ({:02X}, {:02X})", n, vx, vy);
 
                 // Sets collision to 0
                 self.V[0xF] = 0;
